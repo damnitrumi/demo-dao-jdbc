75f93ebadf9bedbb4f21a7787b4959f839135029
Damn it Rumi <guidepaulasoares@hotmail.com>
2023-11-02 21:54:18 -0300

    findByDepartment implementation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -42,7 +42,7 @@ public class Department implements Serializable{
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
-			return true;
+			return true; 
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
